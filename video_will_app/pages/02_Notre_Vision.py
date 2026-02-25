22963490855b0be9cd9bbbbdb21939e223c03909
ksa2003 <kassim.said.ahmed2003@gmail.com>
2026-02-25 22:48:36 +0100

    Update 02_Notre_Vision.py

--- video_will_app/pages/02_Notre_Vision.py
+++ video_will_app/pages/02_Notre_Vision.py
@@ -2,9 +2,11 @@ import streamlit as st
 from theme import apply_theme, img
 from kidan_content import get_slide
 
-apply_theme("Notre Vision")
+apply_theme("Titre de la page")
 
-s = get_slide(2)
+s = get_slide(X)
 
 if s:
-    img(s.image_path)
+    img(s.image_path, use_container_width=True)
+else:
+    st.error("Slide X introuvable")
