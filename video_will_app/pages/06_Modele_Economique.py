c556dbff0cd11c0593e11f991bbf35a676f2c537
ksa2003 <kassim.said.ahmed2003@gmail.com>
2026-02-26 15:21:01 +0100

    Update 06_Modele_Economique.py

--- video_will_app/pages/06_Modele_Economique.py
+++ video_will_app/pages/06_Modele_Economique.py
@@ -3,9 +3,7 @@ from pathlib import Path
 from theme import apply_theme, img
 
 apply_theme("Modèle économique")
-
 st.title("Modèle économique")
 
-image_path = Path("assets/07_modele_economique.png")
-
-img(image_path)
+ASSETS_DIR = Path(__file__).resolve().parents[1] / "assets"
+img(ASSETS_DIR / "07_modele_economique.png")
