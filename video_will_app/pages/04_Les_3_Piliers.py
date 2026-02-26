3983a680f8d89b7ff30993737c8dd72d6ece1ae5
ksa2003 <kassim.said.ahmed2003@gmail.com>
2026-02-26 15:22:08 +0100

    Update 04_Les_3_Piliers.py

--- video_will_app/pages/04_Les_3_Piliers.py
+++ video_will_app/pages/04_Les_3_Piliers.py
@@ -3,9 +3,10 @@ from pathlib import Path
 from theme import apply_theme, img
 
 apply_theme("Les 3 piliers")
-
 st.title("Les 3 piliers du modèle")
 
-img(Path("assets/09_pilier_juridique.png"))
-img(Path("assets/10_pilier_transmission.png"))
-img(Path("assets/11_pilier_emotion.png"))
+ASSETS_DIR = Path(__file__).resolve().parents[1] / "assets"
+
+img(ASSETS_DIR / "09_pilier_juridique.png")
+img(ASSETS_DIR / "10_pilier_transmission.png")
+img(ASSETS_DIR / "11_pilier_emotion.png")
