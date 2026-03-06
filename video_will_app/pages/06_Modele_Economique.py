64b31e721c9e96e20c698db7cde8b274c84fbf42
ksa2003 <kassim.said.ahmed2003@gmail.com>
2026-03-06 15:27:02 +0100

    Delete video_will_app/pages/06_Modele_Economique.py

--- video_will_app/pages/06_Modele_Economique.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-from pathlib import Path
-from theme import apply_theme, img
-
-apply_theme("Modèle économique")
-st.title("Modèle économique")
-
-ASSETS_DIR = Path(__file__).resolve().parents[1] / "assets"
-img(ASSETS_DIR / "07_modele_economique.png")
