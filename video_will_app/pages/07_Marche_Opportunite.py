1a3f54f5eaf8d8470af8de2ad12b3221e16f13b9
ksa2003 <kassim.said.ahmed2003@gmail.com>
2026-03-06 15:27:36 +0100

    Delete video_will_app/pages/07_Marche_Opportunite.py

--- video_will_app/pages/07_Marche_Opportunite.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-from pathlib import Path
-from theme import apply_theme, img
-
-apply_theme("Marché & opportunité")
-st.title("Marché & opportunité")
-
-ASSETS_DIR = Path(__file__).resolve().parents[1] / "assets"
-img(ASSETS_DIR / "05_marche_opportunite.png")
